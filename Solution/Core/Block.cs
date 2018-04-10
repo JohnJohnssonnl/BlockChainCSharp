af11b9331d7c9a729dad2a54f153bdbbde979206
JohnJohnssonnl <JohnJohnssonnl@gmail.com>
2018-04-10 23:29:51 +0200

    Added new code for the Block and added BlockChain basics

--- Solution/Core/Block.cs
+++ Solution/Core/Block.cs
@@ -9,16 +9,53 @@ namespace BlockChainCSharp.Core
 {
     public class Block
     {
-        private Int64                       blockNumber;
-        private Hash                        parentHash;
-        private Hash                        blockHash;
-        private IList<BlockTransaction>     blockTransactions;  //One block has a list of transactions
+        private     Int64                       blockHeight;
+        private     Hash                        parentHash;
+        private     Hash                        blockHash;
+        private     DateTime                    blockDateTimeStamp;
+        private     IList<BlockTransaction>     blockTransactions;  //One block has a list of transactions
+
+        //Design decision, we do not keep balances of an address, instead the balance should be calculated from all transactions at the moment of sending coins
+        //One may NOT send coins when calc. balance < 0 AND when blockHeight < actual blockheight (messing with the system)
 
         public Boolean IsGenesisBlock()
         {
             //Returns whether we are working with the genesis block
-            return (this.blockNumber == 0 && this.parentHash == null);
+            return (this.blockHeight == 0 && this.parentHash == null);  //Both has to be true to validate as genesis block
         }
 
+        public Block CreateBlock(BlockChain _Chain)
+        {
+            //Create a new block
+
+            blockDateTimeStamp  = DateTime.UtcNow;                              //Always use UTC
+            blockHash           = CalculateHash();                              //Create Hash
+            parentHash          = _Chain.GetBlock(blockHeight - 1).GetHash();
+            //Gets the height of the blockChain when creating a new block
+            blockHeight         = _Chain.GetHeight();
+            blockHeight++;      //Always increment one
+
+            return this;
+        }
+
+        private Hash GetHash()
+        {
+            return this.blockHash;
+        }
+
+        private Hash CalculateHash()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            Sha3Digest digest = new Sha3Digest(256);
+            byte[] bytes = new byte[32];
+
+            rng.GetBytes(bytes);
+
+            digest.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] result = new byte[32];
+            digest.DoFinal(result, 0);
+
+            return new Hash(result);
+        }
     }
 }
---
+++ Solution/Core/BlockChain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainCSharp.Core
+{
+    public class BlockChain
+    {
+        private Block[] ChainedBlocks;
+
+        public Block GetBlock(Int64 _Height)
+        {
+            //Returns requested block
+            return ChainedBlocks[_Height];
+        }
+        public Int64 GetHeight()
+        {
+            //Returns the height of the blockChain (incremented when creating new blocks)
+            return ChainedBlocks.Length;
+        }
+
+        public void GenerateBlock()
+        {
+            Block newBlock = new Block();
+
+            //Add a new block to the chain
+            ChainedBlocks[ChainedBlocks.Length] = newBlock.CreateBlock(this);
+        }
+    }
+}
