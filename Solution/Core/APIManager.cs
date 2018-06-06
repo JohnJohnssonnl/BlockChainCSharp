94cfbae2ad0f15964de7b6ae03de393cc04306af
johnjohnssonnl <johnjohnssonnl@hotmail.com>
2018-06-06 17:18:17 +0200

    Added some messages

---
+++ Solution/Core/APIManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainCSharp.Core
+{
+    public class APIManager
+    {
+        //Should be called by external wallets/tools to handle requests
+
+
+    }
+}
